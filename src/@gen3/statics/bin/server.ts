e0bca91ece01c7ba9b226b2e22b72436fce0dc03
Mingfei Shao <mshao1@uchicago.edu>
2023-01-19 14:20:25 -0600

    fix

--- src/@gen3/statics/bin/server.ts
+++ src/@gen3/statics/bin/server.ts
@@ -16,7 +16,8 @@ function launchServer(configFolder: string) {
   app.use(helmet.default({
     contentSecurityPolicy: {
       directives: {
-        "script-src": ["'self'", "'unsafe-inline'"]
+        "script-src": ["'self'", "'unsafe-inline'", "https://code.jquery.com", "https://cdnjs.cloudflare.com"],
+        "script-src-attr": null,
       },
     }
   }));
