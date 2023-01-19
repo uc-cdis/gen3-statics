5dbffdf33d34d839b1ec82b32dddcb5e3d8abace
Mingfei Shao <mshao1@uchicago.edu>
2023-01-19 15:30:54 -0600

    revert test

--- src/@gen3/statics/bin/server.ts
+++ src/@gen3/statics/bin/server.ts
@@ -16,8 +16,7 @@ function launchServer(configFolder: string) {
   app.use(helmet.default({
     contentSecurityPolicy: {
       directives: {
-        "script-src": null,
-        // "script-src": ["'self'", "'unsafe-inline'", "'unsafe-eval'", "https://code.jquery.com", "https://cdnjs.cloudflare.com", "https://unpkg.com"],
+        "script-src": ["'self'", "'unsafe-inline'", "'unsafe-eval'", "https://code.jquery.com", "https://cdnjs.cloudflare.com", "https://unpkg.com"],
         "script-src-attr": null,
       },
     }
